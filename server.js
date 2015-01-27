320c60648dc8b1d29d2ac310cbf89dbe9c09749d
Kini <kinka96@abv.bg>
2015-01-27 23:36:29 +0200

    f

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var stylus = require('stylus');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV || 'development';
+var port = process.env.PORT || 3030;
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/server/views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(stylus.middleware(
+        {
+            src: __dirname + '/public',
+            compile: function(str, path) {
+                return stylus(str).set('filename', path);
+            }
+        }
+));
+
+app.use(express.static(__dirname + '/public'));
+
+if (env == 'development'){
+     mongoose.connect('mongodb://localhost/library');
+}
+else {
+    mongoose.connect('mongodb://admin:admin@ds027491.mongolab.com:27491/library');
+}
+var db = mongoose.connection;
+
+db.once('open', function(err){
+    if (err){
+        console.log('Database could not be opened: ' + err);
+        return;
+    }
+
+    console.log('Database up and running...')
+});
+
+db.on('error', function(err) {
+    console.log('Database error: ' + err);
+});
+
+app.get('/partial/:partialArea/:partialName', function(req, res){
+    res.render('partials/' + req.params.partialArea + '/' + req.params.partialName)
+});
+app.get('*', function(req, res) {
+	res.render('index');
+});
+
+app.listen(port);
+console.log("Server running on port: " + port);
