19651f50a6b30d8bded1b47518253ca22616d1fd
Kini <kinka96@abv.bg>
2015-01-27 23:27:21 +0200

    Update app.js

--- public/app/app.js
+++ public/app/app.js
@@ -5,11 +5,7 @@ app.config(function($routeProvider, $locationProvider){
 
 	$routeProvider
 		.when('/', {
-<<<<<<< HEAD
 			templateUrl: '/partial/main/home',
-=======
-			templateUrl: '/partial/home',
->>>>>>> 3a604b978fb67c84e7eaa0b59bff361eebac3146
 			controller: 'MainCtrl'
 		})
 });
